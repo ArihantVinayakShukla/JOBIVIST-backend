a3bc4688a94442744600839263b092cecce2e6ce
ArihantVinayakShukla <shuklaarihant0@gmail.com>
2024-08-26 14:15:10 +0530

    Backend Deployment

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+import { catchAsyncErrors } from "./catchAsyncErrors.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userSchema.js";
+
+export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("User is not authenticated", 400));
+  }
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  req.user = await User.findById(decoded.id);
+
+  next();
+});
+
+export const isAuthorized = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler("User is not authorized to access this resource", 403)
+      );
+    }
+    next();
+  };
+};
